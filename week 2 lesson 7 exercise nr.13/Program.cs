7e5eeb39ec778bd8c1b23ce85dad99690d6e6918
konrad101296 <161070317+konrad101296@users.noreply.github.com>
2024-03-06 17:17:14 +0100

    Update Program.cs
    dodano opcje nie dziel przez zero

--- week 2 lesson 7 exercise nr.13/Program.cs
+++ week 2 lesson 7 exercise nr.13/Program.cs
@@ -1,25 +1,29 @@
-﻿Console.WriteLine("podaj pierwsza liczbe:");
-double liczba1 = double.Parse(Console.ReadLine());
+Console.WriteLine("podaj pierwsza liczbe:");
+double number1 = double.Parse(Console.ReadLine());
 Console.WriteLine("podaj druga liczbe:");
-double liczba2 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine());
 Console.WriteLine("podaj jaka operacje chcesz wykonac:\n1.dodawanie\n2.odejmowanie\n3.mnozenie\n4.dzielenie");
-int wybor = int.Parse(Console.ReadLine());
+int choice = int.Parse(Console.ReadLine());
 
-if (wybor == 1)
+if (choice == 1)
 {
-    Console.WriteLine($"Twoj wynik to: {liczba1 + liczba2}");
+    Console.WriteLine($"Twoj wynik to: {number1 + number2}");
 }
-else if (wybor == 2)
+else if (choice == 2)
 {
-    Console.WriteLine($"Twoj wynik to: {liczba1 - liczba2}");
+    Console.WriteLine($"Twoj wynik to: {number1 - number2}");
 }
-else if (wybor == 3)
+else if (choice == 3)
 {
-    Console.WriteLine($"Twoj wynik to: {liczba1 * liczba2}");
+    Console.WriteLine($"Twoj wynik to: {number1 * number2}");
 }
-else if (wybor == 4)
+else if (choice == 4 && (number1 == 0 || number2 == 0))
 {
-    Console.WriteLine($"Twoj wynik to: {liczba1 / liczba2}");
+    Console.WriteLine("Nie dziel przez zero");
+}
+else if (choice == 4)
+{
+    Console.WriteLine($"Twoj wynik to: {number1 / number2}");
 }
 else
 {
